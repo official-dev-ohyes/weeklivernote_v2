b5c1e85682ac7781cd9d2f8dc11cd4c6d6197109
김은비 <eunbi.kim.kor@gmail.com>
2023-10-22 21:54:20 +0900

    ✨ Feat: 스크린 간 내비게이션 추가

--- frontend/SoolSool/App.tsx
+++ frontend/SoolSool/App.tsx
@@ -1,20 +1,91 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Constants from "expo-constants";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import LoginScreen from "./screens/LoginScreen";
+import AddInfoScreen from "./screens/AddInfoScreen";
+import HomeScreen from "./screens/HomeScreen";
+import CalendarScreen from "./screens/CalendarScreen";
+import MapScreen from "./screens/MapScreen";
+import MyPageScreen from "./screens/MyPageScreen";
+
+const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <BottomTab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="md-home-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="md-calendar-sharp" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="MyPage"
+        component={MyPageScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SoolSool</Text>
+    <SafeAreaView style={styles.rootScreen}>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="AddInfo" component={AddInfoScreen} />
+          <Stack.Screen name="BottomTab" component={BottomTabNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
   },
 });
--- frontend/SoolSool/screens/LoginScreen.tsx
+++ frontend/SoolSool/screens/LoginScreen.tsx
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-function LoginScreen() {
+function LoginScreen({ navigation }) {
+  const navigateToAddInfo = () => {
+    navigation.navigate("AddInfo");
+  };
+  const navigateToBottomTab = () => {
+    navigation.navigate("BottomTab");
+  };
+
   return (
     <View>
       <Text>Login</Text>
+      <Button title="추가 정보 입력" onPress={navigateToAddInfo} />
+      <Button title="메인으로" onPress={navigateToBottomTab} />
     </View>
   );
 }
