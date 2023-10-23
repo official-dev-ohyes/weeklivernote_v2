150d503fa071c3fc30a2a315d22b5ae239170f61
유혜민 <hyemco@gmail.com>
2023-10-23 11:57:12 +0900

    :tada: Init: Backend 프로젝트 생성

---
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/SoolSoolApplication.java
@@ -0,0 +1,13 @@
+package com.ohyes.soolsool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoolSoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoolSoolApplication.class, args);
+	}
+
+}
---
+++ backend/SoolSool/src/test/java/com/ohyes/soolsool/SoolSoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohyes.soolsool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoolSoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
