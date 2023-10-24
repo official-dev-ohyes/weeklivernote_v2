d37acce89cee156a81b1b44168fb03aae8e18703
신세영 <ssy07312@naver.com>
2023-10-24 10:48:07 +0900

    ✨ Feat: DrinkController Annotation 추가
    Change-Id: I54fa12610baccbdf0747bbfe4e69c9c30185bf7e

--- backend/SoolSool/src/main/java/com/ohyes/soolsool/drink/api/DrinkController.java
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/drink/api/DrinkController.java
@@ -1,5 +1,13 @@
 package com.ohyes.soolsool.drink.api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
 public class DrinkController {
+
 
 }
