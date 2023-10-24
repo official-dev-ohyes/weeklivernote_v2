55f16d9d4ab178240d85ce4fccd1acf252f84c2e
박하윤 <the.day0619@gmail.com>
2023-10-24 17:54:25 +0900

    ✨ Feat: login 기초세팅
    Change-Id: I062e23828389781dbefb8b36795b13eea4010d06

---
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/user/application/UserService.java
@@ -0,0 +1,12 @@
+package com.ohyes.soolsool.user.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class UserService {
+
+}
---
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/user/domain/User.java
@@ -0,0 +1,5 @@
+package com.ohyes.soolsool.user.domain;
+
+public class User {
+
+}
