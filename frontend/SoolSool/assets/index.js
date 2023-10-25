740215986115f4f4135942344dd3aecad9b1f424
김은비 <eunbi.kim.kor@gmail.com>
2023-10-25 15:16:21 +0900

    🍱 Asset: 메인 화면용 임시 이미지 추가
    Change-Id: I310329177e6ead8e7cae7a34712fef7314d7388f

---
+++ frontend/SoolSool/assets/index.js
@@ -0,0 +1,14 @@
+import soberImage from "./Home/drunken-level-00.png";
+import mildImage from "./Home/drunken-level-01.png";
+import moderateImage from "./Home/drunken-level-02.png";
+import highImage from "./Home/drunken-level-03.png";
+import veryHighImage from "./Home/drunken-level-04.png";
+import severeImage from "./Home/drunken-level-05.png";
+export {
+  soberImage,
+  mildImage,
+  moderateImage,
+  highImage,
+  veryHighImage,
+  severeImage,
+};
