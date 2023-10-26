b58f7de917171338cb49efb9dbefd09f0603fe05
신세영 <ssy07312@naver.com>
2023-10-26 11:59:17 +0900

    ✨ Feat: swagger 문서 관련 수정
    Change-Id: I3ce5627afe36723d03a5dfdb3a987df5b0f2bb93

--- backend/SoolSool/src/main/java/com/ohyes/soolsool/drink/dto/DrinkInfo.java
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/drink/dto/DrinkInfo.java
@@ -1,5 +1,6 @@
 package com.ohyes.soolsool.drink.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,13 @@ import lombok.Setter;
 @Setter
 public class DrinkInfo {
 
+    @Schema(example = "소주")
     private String category;
+
+    @Schema(example = "잔")
     private String drinkUnit;
+
+    @Schema(type = "integer", format = "int32", example = "1")
     private byte drinkAmount;
 
     @Builder
