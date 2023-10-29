6b82773e62d897a2c566f4059dc3e8e3bb469551
I-HYEON <tjwn1408@naver.com>
2023-10-29 19:59:27 +0900

    ✨ Feat: AddInfo 3단계 페이지 생성

---
+++ frontend/SoolSool/screens/AddInfoSecondScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function AddInfoSecondScreen() {
+  return (
+    <View>
+      <Text>AddInfoSecondScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AddInfoSecondScreen;
---
+++ frontend/SoolSool/screens/AddInfoThirdScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function AddInfoThirdScreen() {
+  return (
+    <View>
+      <Text>AddInfoThirdScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AddInfoThirdScreen;
