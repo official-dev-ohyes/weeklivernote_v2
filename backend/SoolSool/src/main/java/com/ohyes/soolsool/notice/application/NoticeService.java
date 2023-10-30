8d10dddb18bc107fe0545ba924d88c926fba5554
신세영 <ssy07312@naver.com>
2023-10-30 10:21:38 +0900

    ✨ Feat: 공지사항 전체 조회 기능 추가
    Change-Id: If543bc09924a19f8cbcc60c06a366c39dc734897

---
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/notice/application/NoticeService.java
@@ -0,0 +1,37 @@
+package com.ohyes.soolsool.notice.application;
+
+import com.ohyes.soolsool.notice.dao.NoticeRepository;
+import com.ohyes.soolsool.notice.domain.Notice;
+import com.ohyes.soolsool.notice.dto.NoticeDetail;
+import com.ohyes.soolsool.notice.dto.NoticeResponseDto;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    public NoticeResponseDto noticeGet() {
+        List<Notice> notices = noticeRepository.findAll();
+        List<NoticeDetail> noticeDetails = new ArrayList<>();
+
+        notices.forEach(e -> {
+            NoticeDetail noticeDetail = NoticeDetail.builder()
+                .title(e.getTitle())
+                .content(e.getContent())
+                .build();
+
+            noticeDetails.add(noticeDetail);
+        });
+
+        return NoticeResponseDto.builder()
+            .notices(noticeDetails)
+            .build();
+    }
+}
