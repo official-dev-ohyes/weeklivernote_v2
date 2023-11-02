c0cf51655b6bfc579d20f245b88ca906aa84c40b
유혜민 <hyemco@gmail.com>
2023-11-03 01:51:57 +0900

    ✨ Feat: Swagger 토큰 인증 방식 적용
    Change-Id: I99d7cffd913d2e33cb1f4d83ef7e10b78c05650c

--- backend/SoolSool/src/main/java/com/ohyes/soolsool/config/SwaggerConfig.java
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.ohyes.soolsool.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,18 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+            .name("Authorization")
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER);
+
         return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+            .components(
+                new io.swagger.v3.oas.models.Components().addSecuritySchemes("Authorization",
+                    securityScheme))
             .info(new Info()
                 .title("SoolSool Application API Document")
                 .description("SoolSool Application의 API 명세서입니다.")
