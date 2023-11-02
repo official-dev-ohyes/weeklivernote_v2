921df52acaacd1fab5fc156b6bda057ea1ddf270
이소정 <chadireoroonu@gmail.com>
2023-11-02 10:09:10 +0900

    ✨ Feat: 술 기록 추가
    - 초기화 버튼 임시 도입
    - 첫 줄 4개로 입력 표시 개수 제한
    - 향후 아이콘으로 변경 등 수정 예정
    
    Change-Id: I2c58cc9ac518b7d311a80efcf5737a1dcc50ff15

--- frontend/SoolSool/components/Calendar/NowAddedAlcohols.tsx
+++ frontend/SoolSool/components/Calendar/NowAddedAlcohols.tsx
@@ -2,7 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 import React, { useEffect, useState } from "react";
 
 function NowAddedAlcohols({ alcoholRecord }) {
+  const [count, setCount] = useState(0);
   useEffect(() => {
+    setCount(alcoholRecord.length);
     for (let i = 0; i < alcoholRecord.length; i++) {
       console.log(`넘어온 정보${i + 1}번째!`);
       console.log("술 종류:", alcoholRecord[i].category);
@@ -10,9 +12,35 @@ function NowAddedAlcohols({ alcoholRecord }) {
       console.log("단위:", alcoholRecord[i].unit);
     }
   }, [alcoholRecord]);
+
+  console.log(`지금 넘어온 술 기록 개수는 ${count}개`);
+
   return (
     <View style={styles.total}>
-      <View></View>
+      <View style={styles.oneline}>
+        {alcoholRecord.slice(0, 4).map((record, index) => (
+          <View key={index} style={styles.alcoholInfo}>
+            <Text>
+              {record.category}
+              <Text> ({record.unit}) </Text>
+            </Text>
+            <Text>{record.amount}</Text>
+          </View>
+        ))}
+      </View>
+      {count > 4 && (
+        <View style={styles.oneline}>
+          {alcoholRecord.slice(4).map((record, index) => (
+            <View key={index} style={styles.alcoholInfo}>
+              <Text>
+                {record.category}
+                <Text> ({record.unit}) </Text>
+              </Text>
+              <Text>{record.amount}</Text>
+            </View>
+          ))}
+        </View>
+      )}
     </View>
   );
 }
@@ -21,6 +49,17 @@ const styles = StyleSheet.create({
   total: {
     flex: 1,
     // backgroundColor: "black",
+  },
+  oneline: {
+    flex: 0.5,
+    flexDirection: "row",
+    // backgroundColor: "blue",
+  },
+  alcoholInfo: {
+    margin: "1%",
+    padding: "1%",
+    backgroundColor: "pink",
+    flexDirection: "row",
   },
 });
 
