d0fb953ace02a6f0e1220ee5f6c500e106a9c67d
박하윤 <the.day0619@gmail.com>
2023-11-02 22:39:03 +0900

    👷 Chore: 사용자 인증 설정 파일 수정
    Change-Id: I061343116cefaec5471da2240a00e6ee781ead15

--- backend/SoolSool/src/main/java/com/ohyes/soolsool/config/SecurityConfig.java
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.ohyes.soolsool.config;
 
+import com.ohyes.soolsool.user.dao.UserRepository;
+import com.ohyes.soolsool.util.jwt.CustomAuthenticationEntryPoint;
+import com.ohyes.soolsool.util.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -16,6 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 public class SecurityConfig {
 
     private final CorsFilter corsFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // Spring Security 검사 비활성화
     @Bean
@@ -27,7 +32,7 @@ public class SecurityConfig {
     // HTTP 요청에 대한 인증 및 권한 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+        http
             .addFilter(corsFilter)
             .csrf().disable()
             .sessionManagement()
@@ -39,6 +44,12 @@ public class SecurityConfig {
 //            .oauth2Login()
             .anyRequest().permitAll()   // 모든 요청에 모든 사용자 접근 허용(개발 편의를 위해 설정)
 //                .anyRequest().authenticated()   // 모든 요청에 대해 인증 필요
-            .and().build();
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
