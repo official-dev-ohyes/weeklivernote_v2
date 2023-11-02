ca56b5c032d38c6130344e3c6d8102f79890048c
신세영 <ssy07312@naver.com>
2023-11-02 11:16:28 +0900

    🐛 Fix: 기본 타임존 설정
    Change-Id: I9d9f83ff4233b165c30a9e6c4da1cf2041fe8bff

--- backend/SoolSool/src/main/java/com/ohyes/soolsool/SoolSoolApplication.java
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/SoolSoolApplication.java
@@ -2,6 +2,8 @@ package com.ohyes.soolsool;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -18,4 +20,8 @@ public class SoolSoolApplication {
 		SpringApplication.run(SoolSoolApplication.class, args);
 	}
 
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
