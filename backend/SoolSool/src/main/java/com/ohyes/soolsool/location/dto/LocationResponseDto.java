c89399c7453392c07a2927d6cdc1d990ef41dcfc
신세영 <ssy07312@naver.com>
2023-11-13 17:26:33 +0900

    ✨ Feat: 막차 정류장 좌표 반환값 추가
    Change-Id: I666b19e86ad443183816681921b6a72a94422ca5

--- backend/SoolSool/src/main/java/com/ohyes/soolsool/location/dto/LocationResponseDto.java
+++ backend/SoolSool/src/main/java/com/ohyes/soolsool/location/dto/LocationResponseDto.java
@@ -1,6 +1,8 @@
 package com.ohyes.soolsool.location.dto;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,10 +13,13 @@ public class LocationResponseDto {
 
     private String alarmTime;
     private JsonNode shortRoute;
+    private List<Map<String, List<Double>>> subPaths;
 
     @Builder
-    public LocationResponseDto(String alarmTime, JsonNode shortRoute) {
+    public LocationResponseDto(String alarmTime, JsonNode shortRoute,
+        List<Map<String, List<Double>>> subPaths) {
         this.alarmTime = alarmTime;
         this.shortRoute = shortRoute;
+        this.subPaths = subPaths;
     }
 }
