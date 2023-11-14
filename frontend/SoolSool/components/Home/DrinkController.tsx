848463bb8d240a1401a479669893bd7195136267
유혜민 <hyemco@gmail.com>
2023-11-14 17:34:05 +0900

    ✨ Feat: 막차 알림 기능 추가
    Change-Id: I46eec6f83ef0523e74f91b492f06f62baf7e299b

--- frontend/SoolSool/components/Home/DrinkController.tsx
+++ frontend/SoolSool/components/Home/DrinkController.tsx
@@ -250,7 +250,11 @@ const DrinkController: React.FC<DrinkControllerProps> = ({
 
 		// 권한 허용 안함/첫 요청일 경우 권한 허용 Alert
 		if (isPermissionDenied && newValue === 1) {
-			await locationPermissionAlert();
+			const isPermissionAccept = await locationPermissionAlert();
+      if (isPermissionDenied) {
+        
+      }
+
 		}
 
     const keepUpdateLocation = JSON.parse((await AsyncStorage.getItem("keepUpdateLocation")) || "true");
