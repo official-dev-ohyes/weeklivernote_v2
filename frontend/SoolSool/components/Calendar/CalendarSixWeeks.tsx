1bb6b659d24e204706ae8c04c19b32b2608231c4
김은비 <eunbi.kim.kor@gmail.com>
2023-11-16 00:12:35 +0900

    🎨 Style: 캘린더 코드 간소화 및 css 변경
    Change-Id: I0a1168341169786423b5bdec3263be684c3c47b7

--- frontend/SoolSool/components/Calendar/CalendarSixWeeks.tsx
+++ frontend/SoolSool/components/Calendar/CalendarSixWeeks.tsx
@@ -1,43 +1,39 @@
+import React, { useEffect } from "react";
+import { useState, useRef, useMemo, useCallback } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { FAB } from "react-native-paper";
+import { useFocusEffect } from "@react-navigation/native";
+import { useQuery, useQueryClient } from "react-query";
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
 } from "@gorhom/bottom-sheet";
-import React from "react";
-import { useState, useRef, useMemo, useCallback } from "react";
+import { ImageBackground } from "expo-image";
 import { Calendar } from "react-native-calendars";
+import "./LocaleConfig"; // 달력 서식
+
 import DailySummary from "./DailySummary";
 import { fetchMonthRecord } from "../../api/drinkRecordApi";
-import { useFocusEffect } from "@react-navigation/native";
-import { useQuery, useQueryClient } from "react-query";
-import { ImageBackground } from "expo-image";
-import "./LocaleConfig"; // 달력 서식
 import { adaptiveIcon } from "../../assets";
-import { FAB } from "react-native-paper";
+import { getRealToday } from "../../utils/timeUtils";
 
 function CalendarSixWeeks({ navigation }) {
   // 진짜 오늘 정보 저장
-  const today = new Date();
-  const nowDate = `${today.getFullYear()}-${
-    today.getMonth() + 1 < 10 ? "0" : ""
-  }${today.getMonth() + 1}-${
-    today.getDate() < 10 ? "0" : ""
-  }${today.getDate()}`;
+  const nowDate = getRealToday();
 
   const [currentDay, setCurrentDay] = useState("");
   const [isFuture, setIsFuture] = useState<boolean>(false);
   const [selectDay, setSelectDay] = useState("");
   const [isSelectDay, setIsSelectDay] = useState<boolean>(false);
-  const [alcoholDays, setAlcoholDays] = useState({});
-  const [isSame, setIsSame] = useState<boolean>(false);
+  const [alcoholDays, setAlcoholDays] = useState([]);
   const [renderFlag, setRenderFlag] = useState<boolean>(true);
   const queryClient = useQueryClient();
   const tempDay = currentDay ? currentDay : nowDate;
 
   const {
     data: MonthlyData,
-    isLoading: monthlyLoading,
-    isError: monthlyError,
+    isLoading,
+    isError,
   } = useQuery(
     ["MonthlyQuery", renderFlag],
     async () => {
@@ -50,12 +46,10 @@ function CalendarSixWeeks({ navigation }) {
     {
       onSuccess: (data) => {
         if (data) {
-          const tempDays = {};
+          const tempDays = [];
           for (let i = 0; i < data.drinks.length; i++) {
             const tempDate = data.drinks[i].date;
-            tempDays[tempDate] = {
-              marked: true,
-            };
+            tempDays.push(tempDate);
           }
           setAlcoholDays(tempDays);
         }
@@ -122,12 +116,6 @@ function CalendarSixWeeks({ navigation }) {
         await fetchMonthRecord(clickDay);
       }
 
-      if (nowDate === clickDay) {
-        setIsSame(true);
-      } else {
-        setIsSame(false);
-      }
-
       setIsSelectDay(true);
       handlePresentModalPress();
     }
@@ -176,13 +164,14 @@ function CalendarSixWeeks({ navigation }) {
           onPressArrowLeft={handlePressArrowLeft}
           onPressArrowRight={handelPressArrowRight}
           style={styles.calenderStyle}
+          // disableAllTouchEventsForDisabledDays
+          // onMonthChange={(date) => {
+          //   console.log(169, date);
+          // }}
           renderHeader={(date) => {
-            const header = date.toString("MMMM yyyy");
-            const [month, year] = header.split(" ");
-
             return (
               <Text style={styles.calendarHeader}>
-                {year}년 {month}
+                {date.toString("yyyy년 MMMM")}
               </Text>
             );
           }}
@@ -195,11 +184,14 @@ function CalendarSixWeeks({ navigation }) {
                 onPress={() => {
                   handleDayPress(date);
                 }}
-                style={[styles.calendarCell, isDisabled && { opacity: 0.65 }]}
+                style={[
+                  styles.calendarCell,
+                  isDisabled && { opacity: 0.65, paddingLeft: 0 },
+                ]}
               >
                 <View style={styles.calendarCell}>
                   <Text style={styles.calendarDate}>{date.day}</Text>
-                  {alcoholDays[alcoholKey] ? (
+                  {alcoholDays.includes(alcoholKey) ? (
                     <ImageBackground
                       source={adaptiveIcon}
                       style={styles.calendarStamp}
@@ -230,21 +222,15 @@ function CalendarSixWeeks({ navigation }) {
                 />
               ) : (
                 <View style={styles.tempBox}>
-                  <View style={styles.headerBox}>
-                    <Text style={styles.headerText}>{selectDay}</Text>
-                  </View>
-                  {isSame ? (
-                    <View style={styles.dailySummaryTotal}>
-                      <Text style={styles.innerText}>
-                        내일 새벽 5시에 업데이트 됩니다
-                      </Text>
-                    </View>
+                  <Text style={styles.headerText}>{selectDay}</Text>
+                  {nowDate === selectDay ? (
+                    <Text style={styles.innerText}>
+                      내일 새벽 5시에 업데이트 됩니다.
+                    </Text>
                   ) : isFuture ? (
-                    <View style={styles.dailySummaryTotal}>
-                      <Text style={styles.innerText}>
-                        아직은 기록할 수 없어요
-                      </Text>
-                    </View>
+                    <Text style={styles.innerText}>
+                      아직은 기록할 수 없어요.
+                    </Text>
                   ) : (
                     <FAB
                       icon="plus"
@@ -268,33 +254,6 @@ const styles = StyleSheet.create({
     height: "100%",
     flexDirection: "column",
   },
-  dailySummaryComponent: {
-    flex: 1,
-  },
-  dailySummaryTotal: {
-    height: "75%",
-    justifyContent: "center",
-  },
-  tempBox: {
-    height: "90%",
-  },
-  headerBox: {
-    // summary
-    height: "30%",
-    flexDirection: "row",
-    marginLeft: "5%",
-    alignItems: "center",
-  },
-  innerText: {
-    fontSize: 20,
-    fontFamily: "LineRegular",
-    textAlign: "center",
-  },
-  headerText: {
-    fontSize: 18,
-    color: "#363C4B",
-    fontFamily: "LineRegular",
-  },
   calenderStyle: {
     backgroundColor: "black",
     width: "95%",
@@ -315,19 +274,17 @@ const styles = StyleSheet.create({
     // @@@@@@@@@@@@@@@@@@@@@@@@달력 헤더 맨 위 문구@@@@@@@@@@@@@@@@@@@@@@@@
     fontSize: 18,
     fontFamily: "LineRegular",
-    paddingTop: 10,
-    paddingBottom: 10,
     color: "#fff",
-    paddingRight: 5,
+    textAlign: "center",
   },
   calendarCell: {
-    flexDirection: "column",
     alignItems: "center",
     height: 90, // @@@@@@@@@@@@@@@@@@@@@@@@다시 생각해보자@@@@@@@@@@@@@@@@@@@@@@@@ 이럴거면 스크롤뷰 달아야 함
     width: "100%",
     backgroundColor: "#000", // @@@@@@@@@@@@@@@@@@@@@@@@달력 내부 칸 색상@@@@@@@@@@@@@@@@@@@@@@@@
     marginVertical: -7,
-    paddingTop: 2,
+    paddingTop: 7,
+    paddingLeft: 1,
   },
   calendarDate: {
     color: "white", // @@@@@@@@@@@@@@@@@@@@@@@@달력 내부 글씨 색상@@@@@@@@@@@@@@@@@@@@@@@@
@@ -335,6 +292,26 @@ const styles = StyleSheet.create({
   calendarStamp: {
     height: "83%", // @@@@@@@@@@@@@@@@@@@@@@@@술마신 날 도장@@@@@@@@@@@@@@@@@@@@@@@@
     width: "83%",
+  },
+  dailySummaryComponent: {
+    flex: 1,
+  },
+  tempBox: {
+    height: "90%",
+  },
+  headerText: {
+    marginHorizontal: "5%",
+    fontSize: 18,
+    color: "#363C4B",
+    fontFamily: "LineRegular",
+    textAlign: "left",
+  },
+  innerText: {
+    height: "80%",
+    fontSize: 20,
+    fontFamily: "LineRegular",
+    textAlign: "center",
+    top: "40%",
   },
   fab: {
     alignSelf: "center",
