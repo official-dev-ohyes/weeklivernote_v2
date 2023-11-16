6c5b43fde41eb46065245ce725de778b164835db
김은비 <eunbi.kim.kor@gmail.com>
2023-11-16 15:33:03 +0900

    💄 Design: 캘린더 전구 아이콘 힌트로 변경
    Change-Id: Id6e8af318c2a46cec8327fc37308fd6fa63562b7

--- frontend/SoolSool/screens/CalendarScreen.tsx
+++ frontend/SoolSool/screens/CalendarScreen.tsx
@@ -6,10 +6,10 @@ import {
   Alert,
   Dimensions,
 } from "react-native";
-import React, { useState } from "react";
-import CalendarMain from "../components/Calendar/CalendarMain";
+import React from "react";
+import { IconButton, MD3Colors } from "react-native-paper";
+// import CalendarMain from "../components/Calendar/CalendarMain";
 import CalendarSixWeeks from "../components/Calendar/CalendarSixWeeks";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { subbackground } from "../assets";
 
 function CalendarScreen({ navigation }) {
@@ -21,18 +21,16 @@ function CalendarScreen({ navigation }) {
     >
       <View style={styles.mainBackground}>
         <View style={styles.mainTextBox}>
-          <View style={styles.titleBox}>
-            <Text style={styles.headerText}>술력</Text>
-            {/* 아래 클릭 시 새벽 5시 기준 초기화 정보 띄워주기 */}
-            <MaterialCommunityIcons
-              name="lightbulb-on-outline"
-              size={30}
-              color="white"
-              onPress={() => {
-                Alert.alert("알림", "05시를 기준으로 하루를 초기화합니다.");
-              }}
-            />
-          </View>
+          <Text style={styles.headerText}>술력</Text>
+          <IconButton
+            icon="help-circle-outline"
+            iconColor={MD3Colors.neutral60}
+            size={20}
+            onPress={() => {
+              Alert.alert("알림", "05시를 기준으로 하루를 초기화합니다.");
+            }}
+            style={{ marginLeft: "-1%", marginTop: "7%" }}
+          />
         </View>
         <View style={styles.calendar}>
           {/* 주 별 간격 조정 */}
@@ -56,10 +54,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#121B33",
   },
   mainTextBox: {
-    height: "13%",
+    height: "10%",
     padding: "5%",
-    // paddingBottom: "-3%",
     flexDirection: "row",
+    alignItems: "center",
   },
   headerText: {
     fontSize: 40,
@@ -67,15 +65,8 @@ const styles = StyleSheet.create({
     verticalAlign: "bottom",
     color: "white",
   },
-  titleBox: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
   calendar: {
     height: "87%",
-    // borderWidth: 20,
-    // borderColor: "red",
   },
 });
 
